7db3654038a3eb523a821df484235f1187e91e42
Ahmet Rüchan Dora <ahmetdoraa@gmail.com>
2025-08-01 14:21:41 +0300

    changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import DataTablePage from './pages/DataTable/DataTablePage';
-import Test from './pages/Test/Test';
 
 function App() {
   return (
     <div className="App h-[100vh] justify-center">
-      <Test />
+      <DataTablePage />
     </div>
   );
 }
